0196ef35f16700903956e11ca913b5a75c451b61
Atieh Hamidi <hamidiatiye@gmail.com>
2020-10-17 11:09:39 +0200

    added first shema for main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import QuestionCard from "./components/QuestionCard";
 
 function App() {
-  return <div className="App">Quiz</div>;
+  const start = async () => {};
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+  const nextQuestion = () => {};
+  return (
+    <div className="App">
+      <h1>React Quiz</h1>
+      <button className="start" onClick={start}>
+        Start
+      </button>
+      <p className="score">Score</p>
+      <p> Loading Questions ...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
+  );
 }
 
 export default App;
